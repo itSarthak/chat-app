1d1030c7cbdd0d76a6ac3ff6d0aa227132975fcb
itSarthak <sarthakt3006@gmail.com>
2025-09-20 10:58:30 +0530

    fiX: vercel deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -34,7 +34,6 @@ const App = () => {
         <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
         <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
         <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
-        {/* <Route path="/login" element={<LoginPage />} /> */}
         <Route path="/settings" element={<SettingsPage />} />
         <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
       </Routes>
